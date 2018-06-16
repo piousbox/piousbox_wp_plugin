bdc148f54bba6fbbbbb865ae076fd8d956bccbca
v p <piousbox@vs-MacBook-Pro.local>
2018-06-15 18:05:05 -0700

    commit

--- piousbox_wp_plugin.php
+++ piousbox_wp_plugin.php
@@ -2,13 +2,57 @@
 /**
  * Plugin Name: Piousbox Wordpress Plugin
  */
-//[foobar]
+wp_register_style('myCSS', plugins_url('piousbox_wp_plugin/style.css'));
+wp_enqueue_style( 'myCSS');
+
+/*
+ * [foobar]
+ */
 function foobar_func( $atts ){
   return "foo and bar";
 }
 add_shortcode( 'foobar', 'foobar_func' );
 
-// [feature idx=0]
+
+/*
+ * [category_widget slug='interviewing']
+ */
+function category_widget_shortcode( $raw_attrs ) {
+  $attrs = shortcode_atts( array(
+    'slug' => 'tools'
+  ), $raw_attrs );
+
+  $args = array(
+    'numberposts' => 1,
+    'offset' => $attrs['idx'],
+    'category' => $attrs['slug'],
+    'orderby' => 'post_date',
+    'order' => 'DESC',
+    'post_type' => 'post',
+    'post_status' => 'publish',
+    'suppress_filters' => true
+  );
+  $cat = get_category_by_slug( $attrs['slug'] ); 
+
+  $recent_posts = wp_get_recent_posts( $args, ARRAY_A );
+  $post = $recent_posts[0];
+  $subtitle = new WP_Subtitle( $post['ID'] );
+  $s = $subtitle->get_subtitle();
+
+  $out = <<<EOT
+<div class="CategoryWidget">
+  <h1 class="header">{$cat->name}</h1>
+  <h2><a href="/index.php?p={$post['ID']}">{$post['post_title']}</a></h2>
+  <div class="description"><a href="/index.php?p={$post['ID']}">$s</a></div>
+</div>
+EOT;
+  return $out;
+}
+add_shortcode( 'category_widget', 'category_widget_shortcode' );
+
+/*
+ * [feature idx=0]
+ */
 function feature_shortcode( $raw_attrs ) {
   $attrs = shortcode_atts( array(
     'idx' => 0
@@ -32,8 +76,8 @@ function feature_shortcode( $raw_attrs ) {
   $s = $subtitle->get_subtitle();
   $out = <<<EOT
 <div>
-  <h1>{$post['post_title']}</h1>
-  <div class="description">$s</div
+  <h1><a href="/index.php?p={$post['ID']}">{$post['post_title']}</a></h1>
+  <div class="description"><a href="/index.php?p={$post['ID']}">$s</a></div>
 </div>
 EOT;
   return $out;
