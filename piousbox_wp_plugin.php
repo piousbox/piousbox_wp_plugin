e524d497b0b4b19fbf2962d2359a70113a2c378a
v p <piousbox@vs-MacBook-Pro.local>
2018-07-01 12:29:52 -0700

    n_posts; changelog

--- piousbox_wp_plugin.php
+++ piousbox_wp_plugin.php
@@ -19,33 +19,41 @@ add_shortcode( 'foobar', 'foobar_func' );
  */
 function category_widget_shortcode( $raw_attrs ) {
   $attrs = shortcode_atts( array(
-    'slug' => 'tools'
+    'slug'    => 'tools',
+    'n_posts' => 1
   ), $raw_attrs );
   $cat = get_category_by_slug( $attrs['slug'] );
   # var_dump( $cat );
   $args = array(
-    'numberposts' => 1,
-    'offset' => $attrs['idx'],
-    'category' => $cat->term_id,
-    'orderby' => 'post_date',
-    'order' => 'DESC',
-    'post_type' => 'post',
-    'post_status' => 'publish',
+    'numberposts'      => $attrs['n_posts'],
+    'offset'           => $attrs['idx'],
+    'category'         => $cat->term_id,
+    'orderby'          => 'post_date',
+    'order'            => 'DESC',
+    'post_type'        => 'post',
+    'post_status'      => 'publish',
     'suppress_filters' => true
   );
 
   $recent_posts = wp_get_recent_posts( $args, ARRAY_A );
-  $post = $recent_posts[0];
-  $subtitle = new WP_Subtitle( $post['ID'] );
-  $s = $subtitle->get_subtitle();
+
+  $postsRendered = '';
+  foreach ($recent_posts as &$post) {
+    $subtitle = new WP_Subtitle( $post['ID'] );
+    $s = $subtitle->get_subtitle();
+    $tmp = <<<EOT
+    <div>
+      <h2><a href="/index.php?p={$post['ID']}">{$post['post_title']}</a></h2>
+      <div class="description"><a href="/index.php?p={$post['ID']}">$s</a></div>
+    </div>
+EOT;
+    $postsRendered = "$postsRendered$tmp<br /><br />";
+  }
 
   $out = <<<EOT
-<div class="CategoryWidget">
-  <h1 class="header">{$cat->name}</h1>
-  <h2><a href="/index.php?p={$post['ID']}">{$post['post_title']}</a></h2>
-  <div class="description"><a href="/index.php?p={$post['ID']}">$s</a></div>
-</div>
+    <div class="CategoryWidget"><h1 class="header">{$cat->name}</h1>{$postsRendered}</div>
 EOT;
+
   return $out;
 }
 add_shortcode( 'category_widget', 'category_widget_shortcode' );
@@ -61,13 +69,13 @@ function feature_shortcode( $raw_attrs ) {
   $features_tag = 'Features';
 
   $args = array(
-    'numberposts' => 1,
-    'offset' => $attrs['idx'],
-    'tag' => $features_tag,
-    'orderby' => 'post_date',
-    'order' => 'DESC',
-    'post_type' => 'post',
-    'post_status' => 'publish',
+    'numberposts'      => 1,
+    'offset'           => $attrs['idx'],
+    'tag'              => $features_tag,
+    'orderby'          => 'post_date',
+    'order'            => 'DESC',
+    'post_type'        => 'post',
+    'post_status'      => 'publish',
     'suppress_filters' => true
   );
   $recent_posts = wp_get_recent_posts( $args, ARRAY_A );
