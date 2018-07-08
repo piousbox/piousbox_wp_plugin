26bd79d66be55f5f4b2ea1ece6174ff70cf898f6
v p <piousbox@vs-MacBook-Pro.local>
2018-07-07 18:09:24 -0700

    can display videos on pi

--- piousbox_wp_plugin.php
+++ piousbox_wp_plugin.php
@@ -139,3 +139,70 @@ EOT;
 }
 add_shortcode( 'recent_posts', 'recent_posts_shortcode' );
 
+/**
+ * CategoryVideo Widget
+ * 20180707 _vp_
+ */
+function category_video_widget_shortcode( $raw_attrs ) {
+  $attrs = shortcode_atts( array(
+    'slug'    => 'scrum-diary',
+    'n_posts' => 1
+  ), $raw_attrs );
+  $cat = get_category_by_slug( $attrs['slug'] );
+  $args = array(
+    'numberposts'      => $attrs['n_posts'],
+    'offset'           => $attrs['idx'],
+    'category'         => $cat->term_id,
+    'orderby'          => 'post_date',
+    'order'            => 'DESC',
+    'post_type'        => 'post',
+    'post_status'      => 'publish',
+    'suppress_filters' => true
+  );
+
+  $recent_posts = wp_get_recent_posts( $args, ARRAY_A );
+      
+  $postsRendered = '';
+  foreach ($recent_posts as &$post) {
+    $author   = get_the_author_meta('display_name', $post->author);
+    $date     = substr($post['post_date'], 0, 10);
+    $subtitle = new WP_Subtitle( $post['ID'] );
+    $s        = $subtitle->get_subtitle();
+    $content  = $post['post_content'];
+    $video    = trim( strtok($content, "\n") );
+
+    $pattern = '/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i';
+    preg_match( $pattern, $video, $matches);
+    
+    $content = "<div class='thumb'><img src='https://img.youtube.com/vi/{$matches[1]}/0.jpg' alt='' /></div>";
+
+    $tmp = <<<EOT
+    <div>
+      <h2><a href="/index.php?p={$post['ID']}">{$post['post_title']}</a></h2>
+      <div class="meta" >By $author on {$date}</div>
+      <div class="description">
+        <a href="/index.php?p={$post['ID']}">$content</a><br />
+
+        <ul class="actions">
+          <li>
+            <a href="/index.php?p={$post['ID']}">Play Video</a>
+          </li>
+        </ul>
+
+      </div>
+    </div>
+EOT;
+    $postsRendered = "$postsRendered$tmp<br /><br />";
+  }
+
+  $out = <<<EOT
+    <div class="CategoryVideoWidget">
+      <h1 class="header">{$cat->name}</h1>
+      {$postsRendered}
+    </div>
+EOT;
+
+  return $out;
+}
+add_shortcode( 'category_video_widget', 'category_video_widget_shortcode' );
+
